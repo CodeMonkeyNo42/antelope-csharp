da142daea1df3b7f2ca43c83d94d2c99c25874bc
Bjoern Uhlig <bjoern_uhlig@web.de>
2014-02-11 17:38:04 +0100

    added required nuget packages

---
+++ AntelopeClient/MyBootStrapper.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Practices.Prism.Modularity;
+using Microsoft.Practices.Prism.UnityExtensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace AntelopeClient
+{
+    class MyBootstrapper : UnityBootstrapper
+    {
+        protected override Microsoft.Practices.Prism.Modularity.IModuleCatalog CreateModuleCatalog()
+        {
+            return new ConfigurationModuleCatalog();
+        }
+
+        protected override System.Windows.DependencyObject CreateShell()
+        {
+            return new MainWindow();
+        }
+
+        protected override void InitializeShell()
+        {
+            App.Current.MainWindow = (Window)Shell;
+            App.Current.MainWindow.Show();
+        }
+    }
+}
