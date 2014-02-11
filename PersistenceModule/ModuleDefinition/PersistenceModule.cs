e886fd1282d1e907af333f832b1e13dc07b6616a
Björn Uhlig <bjoern_uhlig@web.de>
2014-02-11 14:25:13 +0100

    added Moduledefinition to PersistenceModule

---
+++ PersistenceModule/ModuleDefinition/PersistenceModule.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Practices.Prism.Modularity;
+using Microsoft.Practices.ServiceLocation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersistenceModule.ModuleDefinition
+{
+    class PersistenceModule : IModule
+    {
+        IServiceLocator ServiceLocator { get; set; }
+
+        PersistenceModule(IServiceLocator serviceLocator)
+        {
+            ServiceLocator = serviceLocator;
+        }
+
+
+        public void Initialize()
+        {
+            // add Views to regions
+            // add Services
+            throw new NotImplementedException();
+        }
+    }
+}
