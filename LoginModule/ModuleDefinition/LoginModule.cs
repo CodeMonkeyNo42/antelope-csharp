881235fe4eff5a2bbc25836ded17a43104e5ccde
Björn Uhlig <bjoern_uhlig@web.de>
2014-02-12 09:42:43 +0100

    added LoginModule

---
+++ LoginModule/ModuleDefinition/LoginModule.cs
@@ -0,0 +1,51 @@
+﻿using LoginModule.Views;
+using Microsoft.Practices.Prism.Modularity;
+using Microsoft.Practices.Prism.Regions;
+using Microsoft.Practices.Unity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginModule.ModuleDefinition
+{
+    public class LoginModule : IModule
+    {
+        IUnityContainer UnityContainer { get; set; }
+        IRegionManager RegionManager { get; set; }
+
+        public LoginModule(IUnityContainer unityContainer, IRegionManager regionManager)
+        {
+            UnityContainer = unityContainer;
+            RegionManager = regionManager;
+        }
+
+
+        public void Initialize()
+        {
+            RegisterServices();
+            RegisterViews();
+            RegisterEvents();
+        }
+
+        void RegisterServices()
+        {
+            // add Services
+            // IMyService myService = ServiceLocator.GetInstance<IMyService>();
+            // or
+            // UnityContainer.RegisterType<IMyService, MyService>();
+        }
+
+        void RegisterViews()
+        {
+            // add Views to regions
+            RegionManager.RegisterViewWithRegion("AntelopeClientLoginRegion", typeof(LoginModuleUserControl));
+        }
+
+        void RegisterEvents()
+        {
+            // register Events to the eventaggregator 
+        }
+    }
+}
