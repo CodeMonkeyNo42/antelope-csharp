2e3984eab60c4dde7ba329e59d507df11b742503
Bjoern Uhlig <bjoern_uhlig@web.de>
2014-02-12 17:17:26 +0100

    added NationModule

---
+++ NationModule/ModuleDefinition/NationModule.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Practices.Prism.Modularity;
+using Microsoft.Practices.Prism.Regions;
+using Microsoft.Practices.Unity;
+using NationModule.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NationModule.ModuleDefinition
+{
+    public class NationModule : IModule
+    {
+        IUnityContainer UnityContainer { get; set; }
+        IRegionManager RegionManager { get; set; }
+
+        public NationModule(IUnityContainer unityContainer, IRegionManager regionManager)
+        {
+            UnityContainer = unityContainer;
+            RegionManager = regionManager;
+        }
+
+
+        public void Initialize()
+        {
+            RegisterServices();
+            RegisterViews();
+            RegisterEvents();
+        }
+
+        void RegisterServices()
+        {
+            // add Services
+            // IMyService myService = ServiceLocator.GetInstance<IMyService>();
+            // or
+            // UnityContainer.RegisterType<IMyService, MyService>();
+        }
+
+        void RegisterViews()
+        {
+            // add Views to regions
+            RegionManager.RegisterViewWithRegion("AntelopeClientTabRegion", typeof(NationModuleTabItemUserControl));
+        }
+
+        void RegisterEvents()
+        {
+            // register Events to the eventaggregator 
+        }
+    }
+}
---
+++ NationModule/Views/NationModuleTabItemUserControl.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NationModule.Views
+{
+    /// <summary>
+    /// Interaktionslogik für NationModuleTabItemUserControl.xaml
+    /// </summary>
+    public partial class NationModuleTabItemUserControl : TabItem
+    {
+        public NationModuleTabItemUserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
