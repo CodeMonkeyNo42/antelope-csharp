4d22f02003982d62f6845e58a78ad854be8b53fa
Bjoern Uhlig <bjoern_uhlig@web.de>
2014-03-16 18:21:37 +0100

    added proper implementation for the ServerCertificateValidationCallback Method

--- PersistenceModule/Api/AntelopeRestApi.cs
+++ PersistenceModule/Api/AntelopeRestApi.cs
@@ -32,7 +32,69 @@ namespace PersistenceModule.Api
             EventAggregator.GetEvent<LoginAndPasswordChangedEvent>().Subscribe(OnLoginAndPasswordChanged);
 
             // to ignore errors from selfcertified servers 
-            ServicePointManager.ServerCertificateValidationCallback += (sender, certificate, chain, sslPolicyErrors) => true;
+            // debug
+            // ServicePointManager.ServerCertificateValidationCallback += (sender, certificate, chain, sslPolicyErrors) => true;
+            // production
+            ServicePointManager.ServerCertificateValidationCallback += CertificateValidationCallBack;
+        }
+
+        /// <summary>
+        /// the implementation of the CertificateValidationCallBack method as shown in MSDN:
+        /// Validating X509 certificates
+        /// http://msdn.microsoft.com/en-us/library/office/dd633677(v=exchg.80).aspx
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="certificate"></param>
+        /// <param name="chain"></param>
+        /// <param name="sslPolicyErrors"></param>
+        /// <returns></returns>
+        private static bool CertificateValidationCallBack(
+         object sender,
+         System.Security.Cryptography.X509Certificates.X509Certificate certificate,
+         System.Security.Cryptography.X509Certificates.X509Chain chain,
+         System.Net.Security.SslPolicyErrors sslPolicyErrors)
+        {
+            // If the certificate is a valid, signed certificate, return true.
+            if (sslPolicyErrors == System.Net.Security.SslPolicyErrors.None)
+            {
+                return true;
+            }
+
+            // If there are errors in the certificate chain, look at each error to determine the cause.
+            if ((sslPolicyErrors & System.Net.Security.SslPolicyErrors.RemoteCertificateChainErrors) != 0)
+            {
+                if (chain != null && chain.ChainStatus != null)
+                {
+                    foreach (System.Security.Cryptography.X509Certificates.X509ChainStatus status in chain.ChainStatus)
+                    {
+                        if ((certificate.Subject == certificate.Issuer) &&
+                           (status.Status == System.Security.Cryptography.X509Certificates.X509ChainStatusFlags.UntrustedRoot))
+                        {
+                            // Self-signed certificates with an untrusted root are valid. 
+                            continue;
+                        }
+                        else
+                        {
+                            if (status.Status != System.Security.Cryptography.X509Certificates.X509ChainStatusFlags.NoError)
+                            {
+                                // If there are any other errors in the certificate chain, the certificate is invalid,
+                                // so the method returns false.
+                                return false;
+                            }
+                        }
+                    }
+                }
+
+                // When processing reaches this line, the only errors in the certificate chain are 
+                // untrusted root errors for self-signed certificates. These certificates are valid
+                // for default Exchange server installations, so return true.
+                return true;
+            }
+            else
+            {
+                // In all other cases, return false.
+                return false;
+            }
         }
 
         private void OnLoginAndPasswordChanged(Tuple<string,string> loginAndPassword)
